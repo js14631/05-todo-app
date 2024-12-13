1f2c7744d25c7f2441deaf9ba4be8c1bdae71793
[Jhorman Serrano] <[jhorman201717@gmail.com]>
2024-12-13 08:37:04 -0500

    Todo model

---
+++ src/todos/models/todo.model.js
@@ -0,0 +1,14 @@
+export class Todo{
+/**
+ * 
+ * @param {String} description 
+ */
+
+    constructor(description){
+        this.id = 1;
+        this.description = description;
+        this.done = false;
+        this.createAT = new Date();
+    }
+
+}
